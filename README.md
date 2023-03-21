26a44446de2fb71527570c06a2e4bb0aa0a4799a
ricardosaracino <ricardosaracino@protonmail.com>
2023-03-21 18:21:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# treemap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ac4yjj-oqkwu7)
---
+++ src/app.component.ts
@@ -0,0 +1,58 @@
+import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, TreemapDescriptionModule } from 'igniteui-angular-core';
+import { CountyHierarchicalDataItem, CountyHierarchicalData } from './CountyHierarchicalData';
+import { IgxPropertyEditorPanelComponent, IgxPropertyEditorPropertyDescriptionComponent } from 'igniteui-angular-layouts';
+import { IgxTreemapComponent } from 'igniteui-angular-charts';
+
+import { defineAllComponents } from 'igniteui-webcomponents';
+
+defineAllComponents();
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+
+    public constructor(private _detector: ChangeDetectorRef) {
+
+    }
+
+    @ViewChild("propertyEditorPanel1", { static: true } )
+    private propertyEditorPanel1: IgxPropertyEditorPanelComponent
+    @ViewChild("layoutTypeEditor", { static: true } )
+    private layoutTypeEditor: IgxPropertyEditorPropertyDescriptionComponent
+    @ViewChild("layoutOrientationEditor", { static: true } )
+    private layoutOrientationEditor: IgxPropertyEditorPropertyDescriptionComponent
+    @ViewChild("headerDisplayModeEditor", { static: true } )
+    private headerDisplayModeEditor: IgxPropertyEditorPropertyDescriptionComponent
+    @ViewChild("labelVerticalAlignmentEditor", { static: true } )
+    private labelVerticalAlignmentEditor: IgxPropertyEditorPropertyDescriptionComponent
+    @ViewChild("treemap", { static: true } )
+    private treemap: IgxTreemapComponent
+
+    private _countyHierarchicalData: CountyHierarchicalData = null;
+    public get countyHierarchicalData(): CountyHierarchicalData {
+        if (this._countyHierarchicalData == null)
+        {
+            this._countyHierarchicalData = new CountyHierarchicalData();
+        }
+        return this._countyHierarchicalData;
+    }
+    
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            TreemapDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+}
+
